453d905fb66388fe2114ab50dae62dff70c314a4
shah20 <shah20@ymail.com>
2019-12-02 21:42:05 +0530

    init

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+
+
+const routes: Routes = [
+  {
+    path: '', component: AppComponent
+  },
+  {
+    path: 'admin', loadChildren: './modules/admin/admin.module#AdminModule'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
